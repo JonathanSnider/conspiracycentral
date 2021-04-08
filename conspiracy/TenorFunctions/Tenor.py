c9348f24d65c6dc5fe65b6d8bcdf47c6035a6520
Jonathan Snider <jtsnider0713@gmail.com>
2021-04-08 11:08:27 -0500

    Made Tenor Class and added tokens.json to .gitignore
    Started to consolidate all Tenor uses into its own class file and added tokens.json to .gitignore

---
+++ conspiracy/TenorFunctions/Tenor.py
@@ -0,0 +1,83 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+from pathlib import Path
+import requests
+import json
+
+
+
+
+class Tenor (commands.Cog):
+  def __init__(self, client):
+    self.client = client
+
+  async def update_morning_gif_file(self): # update the gif file with new gifs
+    # set the api key and limit
+    with open('tokens.json', 'r') as f:
+      tokens = json.load(f)  # app key
+    API_KEY = tokens["tenor"]
+    lmt = 1
+
+    # load the user's anonymous ID from cookies or some other disk storage
+
+    # ELSE - first time user, grab and store their the anonymous ID
+    r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % API_KEY)
+
+    if r.status_code == 200:
+      anon_id = json.loads(r.content.decode('utf-8'))["anon_id"]
+    else:
+      anon_id = ""
+
+    # search term
+    search_term = "cat morning"
+
+    # get the top 8 GIFs for the search term
+    r = requests.get(
+      "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s&anon_id=%s" % (search_term, API_KEY, lmt, anon_id))
+
+    if r.status_code == 200:
+      # load the GIFs using the urls for the smaller GIF sizes
+      top_8gifs = json.loads(r.content.decode('utf-8'))
+      with open('gifs.json', 'w') as outfile:
+        json.dump(top_8gifs, outfile)
+    else:
+      top_8gifs = None
+
+  async def return_gif_from_search(self, search_term, file_name):
+    # set the api key and limit
+    with open('tokens.json', 'r') as f:
+      tokens = json.load(f)  # app key
+    API_KEY = tokens["tenor"]
+    lmt = 1
+
+    # load the user's anonymous ID from cookies or some other disk storage
+
+    # ELSE - first time user, grab and store their the anonymous ID
+    r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % API_KEY)
+
+    if r.status_code == 200:
+      anon_id = json.loads(r.content.decode('utf-8'))["anon_id"]
+    else:
+      anon_id = ""
+    
+    # get the top 8 GIFs for the search term
+    r = requests.get(
+      "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s&anon_id=%s" % (search_term, API_KEY, lmt, anon_id))
+
+    if r.status_code == 200:
+      # load the GIFs using the urls for the smaller GIF sizes
+      top_8gifs = json.loads(r.content.decode('utf-8'))
+      with open(f'{file_name}.json', 'w') as outfile:
+        json.dump(top_8gifs, outfile)
+    else:
+      top_8gifs = None
+
+    with open(f'{file_name}.json', 'r') as gifs_file:
+      gif_urls = json.load(gifs_file)
+    return gif_urls["results"][0]["url"]
+
+def setup(client):
+  n = Tenor(client)
+  client.add_cog(n)  
--- conspiracy/conspiracy.py
+++ conspiracy/conspiracy.py
@@ -6,39 +6,8 @@ from time import gmtime, strftime
 import datetime
 import requests
 import json
+from TenorFunctions.Tenor import Tenor
 client = commands.AutoShardedBot(command_prefix = '!')
-
-async def update_gif_file(): # update the gif file with new gifs
-  # set the api key and limit
-  with open('tokens.json', 'r') as f:
-    tokens = json.load(f)  # app key
-  API_KEY = tokens["tenor"]
-  lmt = 1
-
-  # load the user's anonymous ID from cookies or some other disk storage
-
-  # ELSE - first time user, grab and store their the anonymous ID
-  r = requests.get("https://api.tenor.com/v1/anonid?key=%s" % API_KEY)
-
-  if r.status_code == 200:
-    anon_id = json.loads(r.content.decode('utf-8'))["anon_id"]
-  else:
-    anon_id = ""
-
-  # search term
-  search_term = "cat morning"
-
-  # get the top 8 GIFs for the search term
-  r = requests.get(
-    "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s&anon_id=%s" % (search_term, API_KEY, lmt, anon_id))
-
-  if r.status_code == 200:
-    # load the GIFs using the urls for the smaller GIF sizes
-    top_8gifs = json.loads(r.content.decode('utf-8'))
-    with open('gifs.json', 'w') as outfile:
-      json.dump(top_8gifs, outfile)
-  else:
-    top_8gifs = None
 
 
 @client.event
@@ -50,7 +19,8 @@ async def post_gif():
     current_time = strftime("%H:%M", gmtime())
     if(current_time == '12:00'): # 7am CST
       try: 
-        await update_gif_file()
+        morning_update = Tenor()
+        await morning_update.update_morning_gif_file()
 
         with open('gifs.json', 'r') as gifsFile:
           gif_urls = json.load(gifsFile)
